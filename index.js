464452ae7388c39e3d6d48880c6785cd8b092cb5
Bailey30 <baileymk@hotmail.com>
2025-08-20 16:39:03 +0100

    init

---
+++ index.js
@@ -0,0 +1,127 @@
+import fs from "fs";
+
+class Dictionary {
+  words = [];
+
+  fromFile(filename) {
+    this.words = fs.readFileSync(filename, "utf8").split("\r\n");
+    return this.words;
+  }
+}
+
+class WordSquare {
+  size;
+  input;
+  dict;
+
+  constructor(size, input, dict) {
+    this.size = parseInt(size);
+    this.input = input;
+    this.dict = this.filterPotentialWords(dict);
+
+    this.buildGrid();
+  }
+
+  buildGrid() {
+    for (let i = 0; i < this.dict.length; i++) {
+      let tempInput = this.input;
+
+      const wordGrid = [];
+      wordGrid.push(this.dict[i]);
+
+      if (this.isPotentialWord(this.input, this.dict[i])) {
+        tempInput = this.removeCharsFromInput(tempInput, this.dict[i]);
+        this.getWord(wordGrid, 1, tempInput);
+      }
+    }
+  }
+
+  getWord(wordSquare, row, filteredInput) {
+    if (row === this.size) {
+      wordSquare.forEach((word) => {
+        console.log(word);
+      });
+      console.log("");
+
+      return true;
+    }
+
+    const prefix = this.updatePrefix(wordSquare, row);
+
+    for (let i = 0; i < this.dict.length; i++) {
+      const word = this.dict[i];
+
+      if (
+        this.isPotentialWord(filteredInput, word) &&
+        !wordSquare.includes(word) &&
+        word.startsWith(prefix)
+      ) {
+        const nextSquare = [...wordSquare, word];
+        const nextFiltered = this.removeCharsFromInput(filteredInput, word);
+        if (nextFiltered) {
+          this.getWord(nextSquare, row + 1, nextFiltered);
+        }
+      }
+    }
+  }
+
+  updatePrefix(wordSquare, row) {
+    let prefix = "";
+    for (let r = 0; r < wordSquare.length; r++) {
+      prefix += wordSquare[r][row];
+    }
+
+    return prefix;
+  }
+
+  removeCharsFromInput(chars, word) {
+    const toRemove = word.split("");
+    const charsArr = chars.split("");
+    for (let i = 0; i < charsArr.length; i++) {
+      if (toRemove.includes(charsArr[i])) {
+        charsArr.splice(i, 1);
+        toRemove.splice(toRemove.indexOf(chars[i]), 1);
+      }
+    }
+
+    return charsArr.join("");
+  }
+
+  filterPotentialWords(dict) {
+    const potentialWords = dict
+      .filter((word) => {
+        return this.isPotentialWord(this.input, word);
+      })
+      .sort();
+
+    console.log({ potentialWords });
+    return potentialWords;
+  }
+
+  isPotentialWord(input, word) {
+    // ^: from the first character (so ou can have leading chars that are not a or b)
+    // [ab]: any character which is a or b
+    // +: this found one or more times n succession (can't have other characters in between).
+    // $: until the end (so, you can't have trailing chars that are not a or b)
+    //
+    const regex = new RegExp(`^[${input}]+$`);
+    const potential = word.length === parseInt(this.size) && word.match(regex);
+    return !!potential;
+  }
+}
+
+function main([size, input]) {
+  const dict = new Dictionary();
+  const words = dict.fromFile("dictionary.txt");
+
+  new WordSquare(size, input, words);
+}
+
+if (process.argv.length > 4) {
+  console.error(
+    "The program accepts 2 arguements. The first should be the size of the square. The second should be the characters that fill it.",
+  );
+  console.log("e.g. 4 eeeeddoonnnsssrv");
+} else {
+  main(process.argv.slice(2));
+}
